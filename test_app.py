bac5e3bc6fdf643ab4259919964501935f23df81
Chaneshm <79534745+Chaneshm@users.noreply.github.com>
2022-09-28 17:56:49 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,3 +8,8 @@ from application import app
 def test_home_page():
     response = app.test_client().get('/')
     assert response.status_code == 200
+    
+def testGo():
+    response = app.test_client().get('/go')
+    assert response.status_code == 302
+    assert response.location == 'http://google.com'    
